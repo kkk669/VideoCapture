15a59e9aed9b07a37fa4ee671c9e8a136e785e68
Kenta Kubo <kkk669@users.noreply.github.com>
2022-10-09 16:15:01 +0900

    Add platforms constraints

--- Package.swift
+++ Package.swift
@@ -4,6 +4,7 @@ import PackageDescription
 
 let package = Package(
     name: "VideoCapture",
+    platforms: [.iOS(.v13)],
     products: [
         .library(name: "VideoCapture", targets: ["VideoCapture"]),
     ],
